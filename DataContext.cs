17ed4dd8771e02e3cdf0bb0be3f8424c2f4f8d4e
sahin <sahin.hanay@mobillium.com>
2019-11-03 01:26:51 +0300

    initial commit

---
+++ DataContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerTest
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {}
+    }
+}
